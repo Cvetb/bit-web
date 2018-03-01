fe720caf8c0b0a3a0bddc3bcc4d5905f4dce451b
Branka Cvetic <cveticbranka@yahoo.com>
2018-03-01 23:51:05 +0100

    mario

--- week_5/super_mario/mario.js
+++ week_5/super_mario/mario.js
@@ -1,15 +1,31 @@
+var background = document.querySelector("body");
+var r = document.querySelector("#run");
+var st = document.querySelector("#stay");
+r.classList.add("invisible");
 
-var st = document.querySelector('.stay');
-var r = document.querySelector('.run')
+var run = false;
+var interval;
 
-.onkeyup = function (event) {
-    if (event.keyCode === 39) {
-        r.classList.toggle('stay')
-        st.classList.toggle('run');
-    };
-
+function moveBackground() {
+    var x = 0;
+    interval = setInterval(function () {
+        x -= 10;
+        background.style.backgroundPosition = x + "px 500px";
+    }, 10);
 }
 
+background.addEventListener("keydown", function (event) {
+    if (event.key == "ArrowRight" && run == false) {
+        st.classList.add("invisible");
+        r.classList.add("visible");
+        moveBackground();
+    }
+    run = true;
+});
 
-
-
+background.addEventListener("keyup", function (event) {
+    r.classList.remove("visible");
+    st.classList.remove("invisible");
+    clearInterval(interval);
+    run = false;
+});
