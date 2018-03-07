5b7a7d633855eba3f8dd17c79bbbbca48585fffd
BIT Student <classroom@bgit.rs>
2018-03-07 14:03:48 +0100

    json

---
+++ week_6/JSON and XML/json-practice.js
@@ -0,0 +1,21 @@
+var request = new XMLHttpRequest();
+request.open('GET', 'http://freegeoip.net/json/4.2.2.2', true);
+
+request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        // Success!
+        console.log(request.responseText);
+
+        var data = JSON.parse(request.responseText);
+        console.log(data);
+    } else {
+        // We reached our target server, but it returned an error
+
+    }
+};
+
+request.onerror = function () {
+    // There was a connection error of some sort
+};
+
+request.send();
